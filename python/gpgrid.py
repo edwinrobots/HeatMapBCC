Fixes to BCC heatmap code -- speed, grid priors are now external, variance of grid calculated correctly in bcc

python/gpgrid.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+from scipy.linalg import inv
 
 def sigmoid(f,s):
     g = np.divide(1, 1+np.exp(-s*f))
@@ -7,13 +8,12 @@ def sigmoid(f,s):
 
 def target_var(f,s,v):
     mean = sigmoid(f,s)
-    topvariance = mean*(1-mean)
+    u = mean*(1-mean)
     v = v*s*s
-    prec = 1/v*topvariance**2 + 1/topvariance
-    return np.divide(1,prec)
+    return u/(1/(u*v) + 1)
 
 def latentvariance(std, mean,s):
-    prec = np.divide(1,np.square(std))
+    prec = 1/np.square(std)
     topvariance = np.multiply(mean,(1-mean))
     prec = prec - np.divide(1,topvariance)
     var = np.divide(1,prec)
@@ -23,8 +23,8 @@ def latentvariance(std, mean,s):
 
 class GPGrid(object):
 
-    obs_x = []
-    obs_y = []
+    obsx = []
+    obsy = []
     obs_points = []
     
     obs_f = []
@@ -33,7 +33,7 @@ class GPGrid(object):
     nx = 0
     ny = 0
     
-    nu0 = []
+    #nu0 = []
     
     s = 4 #sigma scaling
     ls = 100 #length-scale
@@ -42,82 +42,81 @@ class GPGrid(object):
     partialK = []
     z = []
     
-    prior_mean = 0.5
-    prior_mean_latent = 0
+    #prior_mean = 0.5
+    #prior_mean_latent = 0
         
     def __init__(self, nx, ny, nu0):
         self.nx = nx
         self.ny = ny  
-        self.nu0 = nu0        
+        #self.nu0 = np.float64(nu0)        
     
     def process_observations( self ):
         if self.obs_points==[]:
             return [],[]
             
-        self.obs_x = np.array(self.obs_x, dtype=np.float64)
-        self.obs_y = np.array(self.obs_y, dtype=np.float64)
+        self.obsx = np.array(self.obsx)
+        self.obsy = np.array(self.obsy)
         if len(self.obs_points.shape)==1 or self.obs_points.shape[1]==1:
             grid_p = np.zeros((self.nx,self.ny))
             grid_all = np.zeros((self.nx,self.ny))
             self.obs_points = np.array(self.obs_points).reshape(-1)
-            for i in range(len(self.obs_x)):
-                grid_p[self.obs_x[i], self.obs_y[i]] += self.obs_points[i]
-                grid_all[self.obs_x[i], self.obs_y[i]] += 1
+            for i in range(len(self.obsx)):
+                grid_p[self.obsx[i], self.obsy[i]] += self.obs_points[i]
+                grid_all[self.obsx[i], self.obsy[i]] += 1
                
             deduped_idxs = np.argwhere(grid_all>0)
-            self.obs_x = deduped_idxs[:,0]
-            self.obs_y = deduped_idxs[:,1]   
+            self.obsx = deduped_idxs[:,0]
+            self.obsy = deduped_idxs[:,1]   
                 
-            presp = grid_p[self.obs_x,self.obs_y]
-            allresp = grid_all[self.obs_x,self.obs_y]
+            presp = grid_p[self.obsx,self.obsy]
+            allresp = grid_all[self.obsx,self.obsy]
             
         elif self.obs_points.shape[1]==2:
             presp = np.array(self.obs_points[:,0]).reshape(self.obs_points.shape[0],1)
             allresp = np.array(self.obs_points[:,1]).reshape(self.obs_points.shape[0],1)
          
-        presp += self.nu0[1]
-        allresp += np.sum(self.nu0)
+        #presp += self.nu0[1]
+        #allresp += np.sum(self.nu0)
             
         self.z = np.divide(presp, allresp)
         self.z = self.z.reshape((self.z.size,1))
             
         return presp, allresp 
     
-    def train( self, obs_x, obs_y, obs_points ):
-        self.obs_x = obs_x
-        self.obs_y = obs_y
+    def train( self, obsx, obsy, obs_points ):
+        self.obsx = obsx
+        self.obsy = obsy
         self.obs_points = obs_points
                 
         presp, allresp = self.process_observations()
         
-        self.prior_mean = self.nu0[1] / np.sum(self.nu0)
-        self.prior_mean_latent = np.log(self.prior_mean/(1-self.prior_mean))
-        prior_var = self.prior_mean*(1-self.prior_mean)/(self.nu0[0]+self.nu0[1]+1)#1/self.nu0[0] + 1/self.nu0[1] #should be dealt with through Q?
+        #self.prior_mean = self.nu0[1] / np.sum(self.nu0)
+        #self.prior_mean_latent = np.log(self.prior_mean/(1-self.prior_mean))
+        #prior_var = self.prior_mean*(1-self.prior_mean)/(self.nu0[0]+self.nu0[1]+1)#1/self.nu0[0] + 1/self.nu0[1] #should be dealt with through Q?
         #prior_var_latent = latentvariance(prior_var, self.prior_mean, s) 
         
-        if self.obs_x==[]:
+        if self.obsx==[]:
             print "What is correct way to apply priors? Adding pseudo-counts will not apply to points that" + \
             "are  not included in training."
-            mPr = self.prior_mean
-            stdPr = np.sqrt(prior_var)        
-            f = self.prior_mean_latent
+            mPr = 0.5#self.prior_mean
+            stdPr = 0.25#np.sqrt(prior_var)        
+            f = 0#self.prior_mean_latent
             var = latentvariance(stdPr, mPr, self.s)
             return f, var, mPr, stdPr
         
         #Update to produce training matrices only over known points
-        ddx = np.float64(np.tile(self.obs_x, (len(self.obs_x),1)).T - np.tile(self.obs_x, (len(self.obs_x),1)));
-        ddy = np.float64(np.tile(self.obs_y, (len(self.obs_y),1)).T - np.tile(self.obs_y, (len(self.obs_y),1)));
+        ddx = np.float64(np.tile(self.obsx, (len(self.obsx),1)).T - np.tile(self.obsx, (len(self.obsx),1)))
+        ddy = np.float64(np.tile(self.obsy, (len(self.obsy),1)).T - np.tile(self.obsy, (len(self.obsy),1)))
         
-        Kx = np.exp( np.divide(-np.square(ddx), self.ls) )
-        Ky = np.exp( np.divide(-np.square(ddy), self.ls) )
+        Kx = np.exp( -ddx**2/self.ls )
+        Ky = np.exp( -ddy**2/self.ls )
         K = Kx*Ky
     
-        f = np.zeros(len(self.obs_x))
-        #K = K + 1e-6 * np.eye(n) # jitter    
+        f = np.zeros(len(self.obsx))
+        K = K + 1e-6 * np.eye(len(K)) # jitter    
         
-        #INCLUDE PRIORS HERE?
-        Pr_est = np.divide( presp+1, allresp+2 )
-        Q = np.diagflat( np.divide(np.multiply(Pr_est, 1-Pr_est), allresp) )
+        Pr_est = (presp+1)/(allresp+2)
+        Q = np.diagflat(Pr_est*(1-Pr_est)/(allresp+2))
         
         converged = False    
         nIt = 0
@@ -125,11 +124,11 @@ class GPGrid(object):
             old_f = f
         
             mean_X = sigmoid(f,self.s)
-            self.G = np.diagflat( self.s*np.multiply(mean_X, (1-mean_X)) )
+            self.G = np.diagflat( self.s*mean_X*(1-mean_X) )
         
-            W = K.dot(self.G.T).dot( np.linalg.inv(self.G.dot(K).dot(self.G.T)+Q) )
+            W = K.dot(self.G.T).dot( inv(self.G.dot(K).dot(self.G.T)+Q) )
         
-            f = self.prior_mean_latent + W.dot(self.G.T).dot(self.z-self.prior_mean) #self.prior_mean_latent + 
+            f = W.dot(self.G.T).dot(self.z-0.5)#self.prior_mean) # + self.prior_mean_latent 
             C = K - W.dot(self.G).dot(K) #possibly needs additional variance term sicne the observation is uncertain?
         
             diff = np.max(np.abs(f-old_f))
@@ -137,11 +136,14 @@ class GPGrid(object):
             #print 'GPGRID diff = ' + str(diff)
             nIt += 1
            
-        self.partialK = self.G.T.dot(np.linalg.inv(self.G.dot(K).dot(self.G.T)+Q) );    
+        self.partialK = self.G.T.dot(np.linalg.inv(self.G.dot(K).dot(self.G.T) + Q) );    
         self.obs_f = f.reshape(-1)
         self.obs_C = C
         v = np.diag(C)
-        return self.obs_f, C, sigmoid(self.obs_f, self.s), target_var(self.obs_f, self.s, v)
+        mPr_tr = sigmoid(self.obs_f, self.s)
+        sdPr_tr = np.sqrt(target_var(self.obs_f, self.s, v))
+        
+        return self.obs_f, C, mPr_tr, sdPr_tr
 
     def post_peaks(self, f, C):     
         v = np.diag(C)
@@ -152,12 +154,12 @@ class GPGrid(object):
     
     def post_grid(self):
         
-        ddx = np.float64(-self.obs_x)
-        ddy = np.float64(-self.obs_y)
+        ddx = np.float64(-self.obsx)
+        ddy = np.float64(-self.obsy)
         
-        f_end = self.G.T.dot(self.z-self.prior_mean) 
+        f_end = self.G.T.dot(self.z-0.5)#self.prior_mean) 
         
-        f = np.zeros((self.nx,self.ny), dtype=np.float64) + self.prior_mean_latent
+        f = np.zeros((self.nx,self.ny), dtype=np.float64)# + self.prior_mean_latent
         C = np.ones((self.nx,self.ny), dtype=np.float64)
                 
         start = time.clock()
@@ -187,21 +189,21 @@ class GPGrid(object):
         return mPr, stdPr, f, C
          
     def post_grid_noloops(self):           
-        ddy = np.arange(self.ny,dtype=np.float64).reshape((self.ny,1)) - np.tile(self.obs_y, (self.ny,1))
+        ddy = np.arange(self.ny,dtype=np.float64).reshape((self.ny,1)) - np.tile(self.obsy, (self.ny,1))
         Ky = np.exp( -ddy**2/self.ls )
           
-        fending = self.G.T.dot(self.z-self.prior_mean)
-        ddx = np.arange(self.nx,dtype=np.float64).reshape((self.nx,1)) - np.tile(self.obs_x, (self.nx,1))
+        fending = self.G.T.dot(self.z-0.5)#self.prior_mean)
+        ddx = np.arange(self.nx,dtype=np.float64).reshape((self.nx,1)) - np.tile(self.obsx, (self.nx,1))
         Kx = np.exp( -ddx**2/self.ls )  
         
         start = time.clock()
     
         Kx = np.tile( Kx, (1,self.ny))
-        Kx = Kx.reshape((self.nx*self.ny, len(self.obs_x)))
+        Kx = Kx.reshape((self.nx*self.ny, len(self.obsx)))
         Ky = np.tile(Ky, (self.nx,1))
         Kpred = Kx*Ky
         W_i = Kpred.dot(self.partialK)  
-        f = self.prior_mean_latent  + W_i.dot(fending).reshape((self.nx,self.ny)) 
+        f = W_i.dot(fending).reshape((self.nx,self.ny))#self.prior_mean_latent  +  
         C = np.ones(self.nx*self.ny)
         Cwg = W_i.dot(self.G)
         for i in np.arange(self.nx*self.ny):

python/heatmapbcc.py
@@ -1,131 +1,10 @@
 
 import ibcc
+from gpgrid import GPGrid
 import numpy as np
 from scipy.special import psi, gammaln
 from scipy.sparse import coo_matrix
 from scipy.linalg import eigh 
-from scipy.stats import norm
-from gpgrid import GPGrid
-    
-def _pinv_1d(v, eps=1e-5):
-    """
-    A helper function for computing the pseudoinverse.
-
-    Parameters
-    ----------
-    v : iterable of numbers
-        This may be thought of as a vector of eigenvalues or singular values.
-    eps : float
-        Elements of v smaller than eps are considered negligible.
-
-    Returns
-    -------
-    v_pinv : 1d float ndarray
-        A vector of pseudo-inverted numbers.
-
-    """
-    return np.array([0 if abs(x) < eps else 1/x for x in v], dtype=float)    
-    
-def _psd_pinv_decomposed_log_pdet(mat, cond=None, rcond=None,
-                                  lower=True, check_finite=True):
-    """
-    SCIPY 0.14
-    Compute a decomposition of the pseudo-inverse and the logarithm of
-    the pseudo-determinant of a symmetric positive semi-definite
-    matrix.
-
-    The pseudo-determinant of a matrix is defined as the product of
-    the non-zero eigenvalues, and coincides with the usual determinant
-    for a full matrix.
-
-    Parameters
-    ----------
-    mat : array_like
-        Input array of shape (`m`, `n`)
-    cond, rcond : float or None
-        Cutoff for 'small' singular values.
-        Eigenvalues smaller than ``rcond*largest_eigenvalue``
-        are considered zero.
-        If None or -1, suitable machine precision is used.
-    lower : bool, optional
-        Whether the pertinent array data is taken from the lower or upper
-        triangle of `mat`. (Default: lower)
-    check_finite : boolean, optional
-        Whether to check that the input matrix contains only finite numbers.
-        Disabling may give a performance gain, but may result in problems
-        (crashes, non-termination) if the inputs do contain infinities or NaNs.
-
-    Returns
-    -------
-    M : array_like
-        The pseudo-inverse of the input matrix is np.dot(M, M.T).
-    log_pdet : float
-        Logarithm of the pseudo-determinant of the matrix.
-
-    """
-    # Compute the symmetric eigendecomposition.
-    # The input covariance matrix is required to be real symmetric
-    # and positive semidefinite which implies that its eigenvalues
-    # are all real and non-negative,
-    # but clip them anyway to avoid numerical issues.
-
-    # TODO: the code to set cond/rcond is identical to that in
-    # scipy.linalg.{pinvh, pinv2} and if/when this function is subsumed
-    # into scipy.linalg it should probably be shared between all of
-    # these routines.
-
-    # Note that eigh takes care of array conversion, chkfinite,
-    # and assertion that the matrix is square.
-    s, u = eigh(mat, lower=lower, check_finite=check_finite)
-
-    if rcond is not None:
-        cond = rcond
-    if cond in [None, -1]:
-        t = u.dtype.char.lower()
-        factor = {'f': 1E3, 'd': 1E6}
-        cond = factor[t] * np.finfo(t).eps
-    eps = cond * np.max(abs(s))
-
-    if np.min(s) < -eps:
-        raise ValueError('the covariance matrix must be positive semidefinite')
-
-    s_pinv = _pinv_1d(s, eps)
-    U = np.multiply(u, np.sqrt(s_pinv))
-    log_pdet = np.sum(np.log(s[s > eps]))
-
-    return U, log_pdet    
-
-_LOG_2PI = np.log(2 * np.pi)
-
-def _loggausspdf(x, mean, cov):
-    """
-    SCIPY 0.14
-    Parameters
-    ----------
-    x : ndarray
-        Points at which to evaluate the log of the probability
-        density function
-    mean : ndarray
-        Mean of the distribution
-    prec_U : ndarray
-        A decomposition such that np.dot(prec_U, prec_U.T)
-        is the precision matrix, i.e. inverse of the covariance matrix.
-    log_det_cov : float
-        Logarithm of the determinant of the covariance matrix
-
-    Notes
-    -----
-    As this function does no argument checking, it should not be
-    called directly; use 'logpdf' instead.
-
-    """
-    prec_U, log_det_cov = _psd_pinv_decomposed_log_pdet(cov)
-    
-    dim = x.shape[-1]
-    dev = x - mean
-    maha = np.sum(np.square(np.dot(dev, prec_U)), axis=-1)
-    return -0.5 * (dim * _LOG_2PI + log_det_cov + maha)    
-
 
 class  Heatmapbcc(ibcc.Ibcc):
     #Interpolating BCC (interBcc)
@@ -171,20 +50,30 @@ class  Heatmapbcc(ibcc.Ibcc):
         self.mPr = {}
         self.mPr[0] = np.ones((self.nx,self.ny))
         
+        nu_rest = []        
         for j in range(1,self.nClasses):
             mPr, sdPr, _, _ = self.heatGP[j].post_grid()
-            self.nu[j,:,:], _ = self.kappa_moments_to_nu(mPr, sdPr)     
-            self.lnKappa[j,:,:] = np.log(mPr)
+            self.nu[j,:,:], totalNu = self.kappa_moments_to_nu(mPr, sdPr)
+            self.nu[j,:,:] += self.nu0[j]
+            totalNu += np.sum(self.nu0)
+            self.lnKappa[j,:,:] = psi(self.nu[j,:,:]) - psi(totalNu)
             self.sd_post_T[j] = sdPr
             self.mPr[j] = mPr
             self.mPr[0] -= mPr
-            
-        self.lnKappa[0,:,:] = np.log(self.mPr[0])
+            if nu_rest==[]:
+                nu_rest = totalNu
+            nu_rest = nu_rest - self.nu[j,:,:]
+                
+        self.nu[0,:,:] = nu_rest             
+        self.lnKappa[0,:,:] = psi(nu_rest) - psi(totalNu)
+        self.sd_post_T[0] = np.sqrt(self.beta_var(self.nu[0,:,:], totalNu-self.nu[0,:,:]))
         self.expecT()        
         return self.ET
     
     def getmean(self, j=1):
+        #return np.exp(self.lnKappa[j,:,:])/np.sum(np.exp(self.lnKappa),axis=0)
         return self.mPr[j]
+        #return self.nu[j,:,:]
 
     def getsd(self, j=1):
         return self.sd_post_T[j]
@@ -201,6 +90,7 @@ class  Heatmapbcc(ibcc.Ibcc):
         self.trainT = trainT
            
     def preprocessCrowdLabels(self, crowdLabels):
+        crowdLabels = np.array(crowdLabels) #ensure we convert any matrix objects or raw arrays
         self.crowdLabels = crowdLabels
         if crowdLabels.shape[1]!=4 or self.crowdTable != None:            
             print 'Must use a sparse list of crowdsourced labels with 4 columns:'
@@ -211,10 +101,10 @@ class  Heatmapbcc(ibcc.Ibcc):
         obsGrid = np.zeros((self.nx,self.ny))
         obsGrid[self.C[:,1], self.C[:,2]] = 1
         obsPairs = np.argwhere(obsGrid>0)
-        self.obsIdx_x = obsPairs[:,0]
-        self.obsIdx_y = obsPairs[:,1]
+        self.obsx = obsPairs[:,0]
+        self.obsy = obsPairs[:,1]
     
-        nobs = len(self.obsIdx_x)
+        nobs = len(self.obsx)
         for j in range(1,self.nClasses):
             self.f_pr[j] = np.tile(self.f_pr[j], nobs)
             self.Cov_pr[j] = np.tile(self.Cov_pr[j], (nobs,nobs))
@@ -244,30 +134,31 @@ class  Heatmapbcc(ibcc.Ibcc):
         totalNu = mPr*(1-mPr)/(sdPr**2) - 1
         nu_j = totalNu*mPr
         return nu_j, totalNu
+    
+    def beta_var(self, nu0, nu1):
+        var = nu0*nu1 / ((nu0+nu1)**2 * (nu0+nu1+1))
+        return var
 
     def expecLnKappa(self):
         nu_rest = []
-        kappa_rest = []
         
         self.f = {}
         self.Cov = {}        
         for j in range(1,self.nClasses):
-            obsPoints = self.ET[j, self.obsIdx_x, self.obsIdx_y]
-            self.f[j], self.Cov[j], mPr, sdPr = self.heatGP[j].train(self.obsIdx_x, self.obsIdx_y, obsPoints)    
+            obsPoints = self.ET[j, self.obsx, self.obsy]
+            self.f[j], self.Cov[j], mPr, sdPr = self.heatGP[j].train(self.obsx, self.obsy, obsPoints)    
             #convert to pseudo-counts
             nu_j, totalNu = self.kappa_moments_to_nu(mPr, sdPr)
-            self.nu[j, self.obsIdx_x, self.obsIdx_y] = nu_j
+            totalNu += np.sum(self.nu0)
+            self.nu[j, self.obsx, self.obsy] = nu_j + self.nu0[j]
             if nu_rest==[]:
-                nu_rest = totalNu*(1-mPr)
-                kappa_rest = 1-mPr
-            else:
-                nu_rest = nu_rest - totalNu*(mPr)
-                kappa_rest = kappa_rest - mPr
+                nu_rest = totalNu
+            nu_rest = nu_rest - self.nu[j, self.obsx, self.obsy]
             
-            self.lnKappa[j, self.obsIdx_x, self.obsIdx_y] = np.log(mPr)
+            self.lnKappa[j, self.obsx, self.obsy] = psi(nu_j)-psi(totalNu)#np.log(mPr)
             
-        self.nu[0, self.obsIdx_x, self.obsIdx_y] = nu_rest
-        self.lnKappa[0, self.obsIdx_x, self.obsIdx_y] = np.log(kappa_rest)
+        self.nu[0, self.obsx, self.obsy] = nu_rest
+        self.lnKappa[0, self.obsx, self.obsy] = psi(nu_rest)-psi(totalNu)#np.log(kappa_rest)
             
     def expecT(self):       
         lnjoint = np.zeros((self.nClasses, self.nx, self.ny))
@@ -281,18 +172,14 @@ class  Heatmapbcc(ibcc.Ibcc):
         
         lnjoint = lnjoint + self.lnKappa
         
-        joint = np.zeros((self.nClasses, self.nx, self.ny))
-        pT = np.zeros((self.nClasses, self.nx, self.ny))
         #ensure that the values are not too small
         largest = np.max(lnjoint, 0)
-        for j in range(self.nClasses):
-            joint[j,:,:] = lnjoint[j,:,:] - largest
+        lnjoint = lnjoint - largest
             
-        joint = np.exp(joint)
+        joint = np.exp(lnjoint)
         norma = np.sum(joint, axis=0)
-        for j in range(self.nClasses):
-            pT[j,:,:] = np.divide(joint[j,:,:], norma)
-            self.ET[j,:,:] = pT[j,:,:]
+        pT = joint/norma
+        self.ET = pT
             
         trainIdxs = self.trainT!=-1
         self.ET[:, trainIdxs] = 0
@@ -305,29 +192,26 @@ class  Heatmapbcc(ibcc.Ibcc):
     def postAlpha(self):#Posterior Hyperparams
         for j in range(self.nClasses):
             for l in range(self.nScores):
-                counts = np.matrix( np.transpose(self.C[:,3]==l) \
-                                    * self.ET[1,self.C[:,1],self.C[:,2]])
+                counts = (self.C[:,3]==l).T.dot(self.ET[j,self.C[:,1],self.C[:,2]])
                 self.alpha[j,l,:] = self.alpha0[j,l] + counts
 
     def postLnJoint(self, lnjoint):
-        ET = self.ET[:, self.obsIdx_x, self.obsIdx_y]
-        lnjoint = lnjoint[:, self.obsIdx_x, self.obsIdx_y]
+        ET = self.ET[:, self.obsx, self.obsy]
+        lnjoint = lnjoint[:, self.obsx, self.obsy]
         lnpCT = np.sum(np.sum(np.sum( np.multiply(lnjoint, ET) )))                        
         return lnpCT
     
     def postLnKappa(self):
-        lnpKappa = 0
-        for j in range(1,self.nClasses):
-            lnpKappa += _loggausspdf(self.f[j].T, self.f_pr[j].T, self.Cov_pr[j])
-        return lnpKappa 
+        lnpKappa = gammaln(np.sum(self.nu0))-np.sum(gammaln(self.nu0)) + \
+                    np.sum( (self.nu0-1).reshape((self.nClasses,1,1))*self.lnKappa, 0)            
+        return np.sum(lnpKappa)
  
-    def qLnKappa(self):        
-        lnqKappa = 0
-        for j in range(1,self.nClasses):
-            lnqKappa += _loggausspdf(self.f[j].T, self.f[j].T, self.Cov[j])
-        return lnqKappa
+    def qLnKappa(self):
+        lnqKappa = gammaln(np.sum(self.nu))-np.sum(gammaln(self.nu)) + \
+                    np.sum( self.nu-1*self.lnKappa, 0)
+        return np.sum(lnqKappa)
         
     def qLnT(self):
-        ET = self.ET[:, self.obsIdx_x, self.obsIdx_y]
+        ET = self.ET[:, self.obsx, self.obsy]
         lnqT = np.sum( np.multiply( ET,np.log(ET) ) )
         return lnqT

python/ushahidiheatmap.py
@@ -3,18 +3,18 @@ Created on 23 Jun 2014
 
 @author: edwin
 '''
+import heatmapbcc
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import heatmapbcc
 from scipy.sparse import coo_matrix
 
 sea_map = []
 
 def runBCC(C,nx,ny,nReporters):
-    nu0 = np.array([0.05, 0.05])#0.03
+    nu0 = np.array([0.5, 0.5])#0.03
     
-    alpha0 = np.array([[3, 1], [1, 3]])
+    alpha0 = np.array([[30.0, 1.0], [1.0, 30.0]])
     if nReporters==2:
         alpha0new = np.zeros((2,2,2))
         alpha0new[:,:,0] = alpha0#[[150, 100], [100, 150]]
@@ -22,7 +22,7 @@ def runBCC(C,nx,ny,nReporters):
         alpha0[:,:,1] = [[1000,1], [1, 1000]]      
     combiner = heatmapbcc.Heatmapbcc(nx, ny, 2, 2, alpha0, nu0, nReporters)
     combiner.minNoIts = 5
-    combiner.maxNoIts = 20
+    combiner.maxNoIts = 200
     bcc_pred = combiner.combineClassifications(C)
     #bcc_pred = np.exp(combiner.lnKappa)
     bcc_pred = bcc_pred[1,:,:].reshape((nx,ny))
@@ -198,13 +198,13 @@ def loadUshData(nx,ny):
                 continue
             
             try:
-                Crow = np.matrix([0, repx, repy, 1]) # all report values are 1 since we only have confirmations of an incident, not confirmations of nothing happening
+                Crow = np.array([0, repx, repy, 1]) # all report values are 1 since we only have confirmations of an incident, not confirmations of nothing happening
             except ValueError:
                 print 'ValueError creating a row of the crowdsourced data matrix.!'        
             if C=={} or typeID not in C:
-                C[typeID] = Crow
+                C[typeID] = Crow.reshape((1,4))
             else:
-                C[typeID] = np.concatenate((C[typeID], Crow), axis=0)      
+                C[typeID] = np.concatenate((C[typeID], Crow.reshape(1,4)), axis=0)      
 
     return nreporters, C   
 
@@ -228,7 +228,7 @@ def writeCoordsToJson(minlat,maxlat,minlon,maxlon):
         
 def insertTrusted(nx,ny,C):
     x,y = translate_points_to_local(18.52,-72.284,nx,ny)
-    Crow = np.matrix([[1, x, y, 0],[1, x+1, y, 0],[1, x+2, y, 0],[1, x+3, y, 0], \
+    Crow = np.array([[1, x, y, 0],[1, x+1, y, 0],[1, x+2, y, 0],[1, x+3, y, 0], \
                     [1, x+1, y+1, 0],[1, x+1, y-1, 0],[1, x+2, y+1, 0],[1, x+3, y-1, 0], \
                     [1, x-1, y+1, 0],[1, x-1, y-1, 0],[1, x+2, y-1, 0],[1, x+3, y+1, 0]]) 
     C[1] = np.concatenate((C[1], Crow), axis=0)
@@ -262,8 +262,8 @@ if __name__ == '__main__':
 #         writeImg("_rep_intensity__sd_",j)   
     
     #Using BCC - lower res so it is tractable to interpolate
-    nx = 1000
-    ny = 1000
+    nx = 200
+    ny = 200
     _, C = loadUshData(nx,ny)       
     for j in range(1,2):
         bcc_pred,combiner = runBCC(C[j],nx,ny,1)