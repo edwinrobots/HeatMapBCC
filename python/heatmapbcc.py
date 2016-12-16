d70d03ee863302bd746d3be8fbaacedb358b9433
edwinrobots <edwin.simpson@gmail.com>
2016-12-16 14:08:16 +0100

    allow more G iterations

--- python/heatmapbcc.py
+++ python/heatmapbcc.py
@@ -131,7 +131,7 @@ class HeatMapBCC(ibcc.IBCC):
             self.heatGP[j].verbose = self.verbose
             self.heatGP[j].max_iter_VB = 1
             self.heatGP[j].min_iter_VB = 1
-            self.heatGP[j].max_iter_G = 1
+#             self.heatGP[j].max_iter_G = 1
             self.heatGP[j].uselowerbound = False # we calculate the lower bound here instead of the GPGrid function
         
     def convergence_measure(self, oldET):
