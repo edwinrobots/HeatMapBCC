fixing part of the lower bound gradient

python/gp_classifier_svi.py
@@ -152,7 +152,8 @@ class GPClassifierSVI(GPClassifierVB):
         fhat = self.um_minus_mu0
         invKs_fhat = (self.invK_mm * self.s).dot(fhat)                
          
-        sigmasq = self.invK_mm.dot(self.K_nm.T).dot(self.G.T).dot(np.diag(self.Q)).dot(self.G).dot(self.K_nm).dot(self.invK_mm)
+        GTQ = (self.G.T * self.Q[ np.newaxis, :])
+        sigmasq = self.invK_mm.dot(self.K_nm.T).dot(GTQ).dot(self.G).dot(self.K_nm).dot(self.invK_mm)
          
         if self.n_lengthscales == 1 or dim == -1: # create an array with values for each dimension
             dims = range(self.obs_coords.shape[1])
@@ -269,7 +270,7 @@ class GPClassifierSVI(GPClassifierVB):
         self.Ks_nm = self.K_nm / self.s            
         self.Ks = self.K / self.s
         
-        self.G = 0 # reset because we will need to compute afresh with new sample    
+        self.G = 0 # reset because we will need to compute afresh with new sample
         self.z_i = self.z[self.data_obs_idx_i]
         self.mu0_i = self.mu0[self.data_idx_i]
         

python/gp_classifier_vb.py
@@ -408,7 +408,7 @@ class GPClassifierVB(object):
         n_obs = obs_values.shape[0]                 
         
         if self.verbose:
-            logging.debug("GP grid for %i observations." % n_obs)
+            logging.debug("GP inference with %i observations." % n_obs)
         
         if not np.any(totals >= 0):
             if (obs_values.ndim==1 or obs_values.shape[1]==1): # obs_value is one column with values of either 0 or 1
@@ -736,7 +736,8 @@ class GPClassifierVB(object):
 
     def _expec_f(self):
         ''' 
-        Compute the expected value of f given current q() distributions for other parameters
+        Compute the expected value of f given current q() distributions for other parameters. Could plug in a different
+        GP implementation here.
         '''
         diff_G = 0
         G_update_rate = 1.0 # start with full size updates